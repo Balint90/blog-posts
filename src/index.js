a5cc536922474dfe11714e705d5014e00ae2ae98
Bálint Csertán <csertanb@gmail.com>
2023-10-05 23:09:23 +0200

    manual duplication (wrong method)

--- src/index.js
+++ src/index.js
@@ -23,6 +23,42 @@ const App = () => {
                     </div>
                 </div>
             </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={profile} alt="profile picture" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sarah
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Today at 5:00pm
+                        </span>
+                    </div>
+                    <div className="text">
+                        it's amazing
+                    </div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={profile} alt="profile picture" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sarah
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Today at 5:00pm
+                        </span>
+                    </div>
+                    <div className="text">
+                        it's amazing
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
